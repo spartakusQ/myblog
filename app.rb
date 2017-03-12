9602e3eefb673bb5f931c625080c7ffbe7678d10
spartakusQ <spartakus1688@gmail.com>
2017-03-12 12:59:31 +0300

    init database

--- app.rb
+++ app.rb
@@ -1,11 +1,30 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sqlite3"
+
+def init_db
+	@db = SQLite3::Database.new 'trollhomeland.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
+
 
 get '/' do
-	erb "Hello!"
+	erb "Hello Troll!"
 end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  content = params[:content]
+
+	erb "You Argh #{content}"
+
 end
