d4abeb7fc0b07cde2611cf22ef868f1626f5f7ff
spartakusQ <spartakus1688@gmail.com>
2017-03-12 17:01:02 +0300

    Save comment to databse

--- app.rb
+++ app.rb
@@ -76,6 +76,19 @@ post '/details/:post_id' do
 
 		content = params[:content]
 
-		erb "You typed comment #{content} for post #{post_id}"
+		@db.execute 'insert into Comments
+			(
+					content,
+					created_date,
+					post_id
+			)
+					values
+			(
+					?,
+					datetime(),
+					?
+			)', [content, post_id]
+
+	redirect to('/details/' + post_id)
 
 end
