fe66a4979b8a32d6302c407e6058c6d7fce2be5d
spartakusQ <spartakus1688@gmail.com>
2017-03-12 16:45:35 +0300

    Add simple /details/ post handler

--- app.rb
+++ app.rb
@@ -50,11 +50,23 @@ post '/new' do
 end
 
 get '/details/:post_id' do
-	post_id = params[:post_id]
 
-	results = @db.execute 'select * from Posts where id = ?', [post_id]
-	@row = results[0]
+		post_id = params[:post_id]
 
-	erb :details
+		results = @db.execute 'select * from Posts where id = ?', [post_id]
+		@row = results[0]
+
+		erb :details
+
+end
+
+#обработчик пост запроса
+post '/details/:post_id' do
+
+		post_id = params[:post_id]
+
+		content = params[:content]
+
+		erb "You typed comment #{content} for post #{post_id}"
 
 end
